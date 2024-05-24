9c88508f0a2d2388523640907ead336d850f4929
byte-engineer <blalalasmayly1998@gmail.com>
2024-05-24 18:16:50 +0400

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,11 +1,18 @@
 from time import time as tm
 from time import sleep as slp
 
-def Is_prime(number):
-    for div in range(2, number, 2):   # Chech if the number is divisible to all the number the less than it,
-        if (number % div) == 0 :      # If it retutn 0 that mean the munber is divisible , So is not prime.
+def Is_prime(Input):                    # negetive values and 0 and 1 are not primes.
+    if Input <= 0 or Input == 1:
+        return False;
+    
+    if Input % 2 == 0 and Input != 2 : # even numbers are not primes
+        return False
+
+    for div in range(2, Input):     # Chech if the Input is divisible to all the Input the less than it
+        if (Input % div) == 0 :        # If it retutn 0 that mean the munber is divisible , So is not prime.
             return False
-    return True                       # Other wise number is prime.
+
+    return True                        # Other wise Input is prime.
 
 
 num_range = int(input("Python script to get a Prime numbers.\nEnter the range >>> "))
@@ -13,15 +20,14 @@ print()
 tm1 = tm()
 i = 0
 for num in range((num_range - 1)):
-
     if Is_prime((num + 2)):
         i += 1
         znum = str((num + 2)).zfill(len(str(num_range)))
         print(f"{znum} --> prime   | {i}")
 
-tm2 = tm()
 
+tm2 = tm()
 print (f"time = {tm2 - tm1}")
 
-print("Quit after 60 seconds")
-slp(60)
+# print("Quit after 60 seconds")
+# slp(60)
